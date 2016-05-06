96b8cffbe0f582b2bd4bfdad1bddc88031b19d05
Michael Critz <michael.critz@sony.com>
2016-05-06 15:37:43 -0700

    created simple view

---
+++ _site/js/main.js
@@ -0,0 +1,13 @@
+require([
+	'jquery',
+	'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js'
+], function(
+		$,
+		_,
+		Backbone
+) {
+	return function() {
+		console.log('options');
+	}
+});
