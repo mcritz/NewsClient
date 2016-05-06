9c5a55ee8a806094f57fe042e095f60d05c91072
Michael Critz <michael.critz@sony.com>
2016-05-06 16:01:58 -0700

    Refactored app to main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,45 @@
 require([
 	'jquery',
 	'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
-	'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js'
+	'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js'
 ], function(
-		$,
-		_,
-		Backbone
+	$,
+	_,
+	Backbone,
+	Handlebars
 ) {
-	return function() {
-		console.log('options');
-	}
+
+var NewsItem = Backbone.Model.extend({
+	title : 'News Flash',
+	content : 'Stay tuned for important news…'
 });
+
+var trumpSucks = new NewsItem({
+	title : 'Oh Boy!',
+	content : 'Trump Chosen as GOP Candidate'
+});
+
+var MainViewClass = Backbone.View.extend({
+	events : {
+		'click' : 'sayHello'
+	},
+	initialize : function() {
+		console.log('hello');
+		this.render();
+ 	},
+ 	render : function() {
+ 		this.$el.html(this.template(this.model.attributes));
+ 	},
+	template : Handlebars.compile($('#newsitem-template').html()),
+ 	sayHello : function() {
+ 		alert('hi!');
+ 	}
+}); // MainView
+
+var helloView = new MainViewClass({
+	el : '#main',
+	model : trumpSucks
+}); // mainView
+
+}); // require
